ec5ddac410ad2925e3595cc917b1950a40bafba7
itsdean <hey@dean.dev>
2020-11-23 11:04:56 +0000

    convertex lf to crlf

--- bin/prepare-packages.sh
+++ bin/prepare-packages.sh
@@ -6,5 +6,4 @@ cd /lambda && pip3 install  -t ./package -r requirements.txt
 cd /lambda/package && zip -r9 /lambda/lambda-package.zip .
 cd /lambda && zip -g lambda-package.zip lambda_function.py pkey.pem github_handler.py slack_handler.py jira_handler.py
 
-# cleanup
 rm -r /lambda/package
